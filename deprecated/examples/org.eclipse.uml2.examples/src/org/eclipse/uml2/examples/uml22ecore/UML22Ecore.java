9d947aa2afb073b0e217b9fbaba29236d54cb417
khussey <khussey>
2005-03-15 18:54:14 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=87373

--- deprecated/examples/org.eclipse.uml2.examples/src/org/eclipse/uml2/examples/uml22ecore/UML22Ecore.java
+++ deprecated/examples/org.eclipse.uml2.examples/src/org/eclipse/uml2/examples/uml22ecore/UML22Ecore.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2003, 2005 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   IBM - Initial API and implementation
+ *   IBM - initial API and implementation
  *
- * $Id: UML22Ecore.java,v 1.7 2005/01/19 22:57:34 khussey Exp $
+ * $Id: UML22Ecore.java,v 1.8 2005/03/15 18:54:14 khussey Exp $
  */
 package org.eclipse.uml2.examples.uml22ecore;
 
