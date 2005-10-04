b2b85483898e9e85fef908d53d8bb58f0e1692dc
khussey <khussey>
2005-10-04 21:55:15 +0000

    [111538] Setting isResolveProxies to false for derived features that return a container.

--- deprecated/plugins/org.eclipse.uml2/src/org/eclipse/uml2/Action.java
+++ deprecated/plugins/org.eclipse.uml2/src/org/eclipse/uml2/Action.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2005 IBM Corporation and others.
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: Action.java,v 1.10 2005/10/04 21:56:43 khussey Exp $
+ * $Id: Action.java,v 1.9 2005/10/04 21:55:15 khussey Exp $
  */
 package org.eclipse.uml2;
 
