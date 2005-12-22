98fd18a9f978a790d727bffc297598a59f085cd0
khussey <khussey>
2005-12-22 20:20:13 +0000

    [80305] Adding UML resources to source control.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/UMLResource.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/UMLResource.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: UMLResource.java,v 1.2 2005/12/19 18:51:32 khussey Exp $
+ * $Id: UMLResource.java,v 1.3 2005/12/22 20:20:13 khussey Exp $
  */
 package org.eclipse.uml2.uml.resource;
 
@@ -60,7 +60,37 @@ public interface UMLResource
 	 */
 	public static final String DEFAULT_ENCODING = "UTF-8"; //$NON-NLS-1$
 
-	public static final String PROFILE_FILE_EXTENSION = "profile." //$NON-NLS-1$
-		+ FILE_EXTENSION;
+	String LIBRARY_FILE_EXTENSION = "library." + FILE_EXTENSION; //$NON-NLS-1$
+
+	String LIBRARIES_PATHMAP = "pathmap://UML_LIBRARIES/"; //$NON-NLS-1$
+
+	String ECORE_PRIMITIVE_TYPES_LIBRARY_URI = LIBRARIES_PATHMAP
+		+ "EcorePrimitiveTypes." + LIBRARY_FILE_EXTENSION; //$NON-NLS-1$
+
+	String JAVA_PRIMITIVE_TYPES_LIBRARY_URI = LIBRARIES_PATHMAP
+		+ "JavaPrimitiveTypes." + LIBRARY_FILE_EXTENSION; //$NON-NLS-1$
+
+	String UML_PRIMITIVE_TYPES_LIBRARY_URI = LIBRARIES_PATHMAP
+		+ "UMLPrimitiveTypes." + LIBRARY_FILE_EXTENSION; //$NON-NLS-1$
+
+	String METAMODEL_FILE_EXTENSION = "metamodel." + FILE_EXTENSION; //$NON-NLS-1$
+
+	String METAMODELS_PATHMAP = "pathmap://UML_METAMODELS/"; //$NON-NLS-1$
+
+	String ECORE_METAMODEL_URI = METAMODELS_PATHMAP + "Ecore." //$NON-NLS-1$
+		+ METAMODEL_FILE_EXTENSION;
+
+	String UML_METAMODEL_URI = METAMODELS_PATHMAP + "UML." //$NON-NLS-1$
+		+ METAMODEL_FILE_EXTENSION;
+
+	String PROFILE_FILE_EXTENSION = "profile." + FILE_EXTENSION; //$NON-NLS-1$
+
+	String PROFILES_PATHMAP = "pathmap://UML_PROFILES/"; //$NON-NLS-1$
+
+	String UML_PROFILE_URI = PROFILES_PATHMAP + "UML." //$NON-NLS-1$
+		+ PROFILE_FILE_EXTENSION;
+
+	String ECORE_PROFILE_URI = PROFILES_PATHMAP + "Ecore." //$NON-NLS-1$
+		+ PROFILE_FILE_EXTENSION;
 
 } //UMLResource
