17476c51636171813fe863b28b826928c6820af9
khussey <khussey>
2006-03-29 22:14:33 +0000

    [105191] Externalizing keywords and labels for profiles.

--- deprecated/examples/org.eclipse.uml2.examples.ui/src/org/eclipse/uml2/examples/ui/actions/ExternalizeKeywordsAction.java
+++ deprecated/examples/org.eclipse.uml2.examples.ui/src/org/eclipse/uml2/examples/ui/actions/ExternalizeKeywordsAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: ExternalizeKeywordsAction.java,v 1.2 2005/12/22 20:19:56 khussey Exp $
+ * $Id: ExternalizeKeywordsAction.java,v 1.3 2006/03/29 22:14:33 khussey Exp $
  */
 package org.eclipse.uml2.examples.ui.actions;
 
@@ -63,7 +63,7 @@ public class ExternalizeKeywordsAction
 				public Object caseStereotype(Stereotype stereotype) {
 					String qualifiedName = stereotype.getQualifiedName();
 
-					if (UML2Util.isEmpty(qualifiedName)) {
+					if (!UML2Util.isEmpty(qualifiedName)) {
 						propertiesWriter.println(getPropertiesKey(
 							UML2Util.EMPTY_STRING, qualifiedName)
 							+ PROPERTIES_SEPARATOR + stereotype.getKeyword());
