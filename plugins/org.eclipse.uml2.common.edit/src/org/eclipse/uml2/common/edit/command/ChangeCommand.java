fc9bee80647e978ca9177e4b7ba73f70b3f42516
khussey <khussey>
2006-04-13 18:32:25 +0000

    [87260] Rebasing change command on new EMF change command.

--- plugins/org.eclipse.uml2.common.edit/src/org/eclipse/uml2/common/edit/command/ChangeCommand.java
+++ plugins/org.eclipse.uml2.common.edit/src/org/eclipse/uml2/common/edit/command/ChangeCommand.java
@@ -8,15 +8,10 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: ChangeCommand.java,v 1.2 2006/03/23 18:53:36 khussey Exp $
+ * $Id: ChangeCommand.java,v 1.3 2006/04/13 18:32:25 khussey Exp $
  */
 package org.eclipse.uml2.common.edit.command;
 
-import java.util.Collections;
-
-import org.eclipse.emf.common.command.AbstractCommand;
-import org.eclipse.emf.ecore.change.ChangeDescription;
-import org.eclipse.emf.ecore.change.util.ChangeRecorder;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.uml2.common.edit.domain.UML2AdapterFactoryEditingDomain;
 
@@ -24,15 +19,11 @@ import org.eclipse.uml2.common.edit.domain.UML2AdapterFactoryEditingDomain;
  * 
  */
 public class ChangeCommand
-		extends AbstractCommand {
+		extends org.eclipse.emf.edit.command.ChangeCommand {
 
 	private final EditingDomain editingDomain;
 
-	private final ChangeRecorder changeRecorder;
-
 	private final Runnable runnable;
-
-	private ChangeDescription changeDescription = null;
 
 	public ChangeCommand(EditingDomain editingDomain, Runnable runnable) {
 		this(editingDomain, runnable, null);
@@ -45,23 +36,23 @@ public class ChangeCommand
 
 	public ChangeCommand(EditingDomain editingDomain, Runnable runnable,
 			String label, String description) {
-		super(label, description);
-
-		if (editingDomain == null) {
-			throw new IllegalArgumentException(String.valueOf(editingDomain));
-		}
+		super(editingDomain.getResourceSet());
 
 		this.editingDomain = editingDomain;
-		this.changeRecorder = editingDomain instanceof UML2AdapterFactoryEditingDomain
-			? ((UML2AdapterFactoryEditingDomain) editingDomain)
-				.getChangeRecorder()
-			: new ChangeRecorder();
+
+		if (editingDomain instanceof UML2AdapterFactoryEditingDomain) {
+			this.changeRecorder = ((UML2AdapterFactoryEditingDomain) editingDomain)
+				.getChangeRecorder();
+		}
 
 		if (runnable == null) {
 			throw new IllegalArgumentException(String.valueOf(runnable));
 		}
 
 		this.runnable = runnable;
+
+		setLabel(label);
+		setDescription(description);
 	}
 
 	public EditingDomain getEditingDomain() {
@@ -72,39 +63,8 @@ public class ChangeCommand
 		return runnable;
 	}
 
-	protected ChangeDescription getChangeDescription() {
-		return changeDescription;
-	}
-
-	protected void setChangeDescription(ChangeDescription changeDescription) {
-		this.changeDescription = changeDescription;
-	}
-
-	protected boolean prepare() {
-		return getChangeDescription() == null;
-	}
-
-	public boolean canUndo() {
-		return getChangeDescription() != null;
-	}
-
-	public void execute() {
-		changeRecorder.beginRecording(Collections.singleton(getEditingDomain()
-			.getResourceSet()));
-
-		try {
-			getRunnable().run();
-		} finally {
-			setChangeDescription(changeRecorder.endRecording());
-		}
-	}
-
-	public void undo() {
-		getChangeDescription().applyAndReverse();
-	}
-
-	public void redo() {
-		getChangeDescription().applyAndReverse();
+	protected void doExecute() {
+		getRunnable().run();
 	}
 
 }
