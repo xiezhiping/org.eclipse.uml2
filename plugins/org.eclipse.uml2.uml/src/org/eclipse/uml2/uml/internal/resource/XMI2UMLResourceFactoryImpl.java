80cef2b0a6619a59c0e814e0f5871222a4913ae9
khussey <khussey>
2006-04-26 16:58:39 +0000

    [80306] Adding support for interchange with UML 2.1 XMI files.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/internal/resource/XMI2UMLResourceFactoryImpl.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/internal/resource/XMI2UMLResourceFactoryImpl.java
@@ -8,21 +8,15 @@
  * Contributors:
  *   IBM - initial API and implementation
  * 
- * $Id: XMI2UMLResourceFactoryImpl.java,v 1.1 2006/04/26 15:48:53 khussey Exp $
+ * $Id: XMI2UMLResourceFactoryImpl.java,v 1.2 2006/04/26 16:58:39 khussey Exp $
  */
 package org.eclipse.uml2.uml.internal.resource;
 
-import java.util.Map;
-
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.ExtendedMetaData;
 import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.ecore.xmi.XMLSave;
 import org.eclipse.uml2.uml.resource.XMI2UMLExtendedMetaData;
 import org.eclipse.uml2.uml.resource.XMI2UMLResource;
 
@@ -48,23 +42,7 @@ public class XMI2UMLResourceFactoryImpl
 
 		resource.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
 
-		Map defaultSaveOptions = resource.getDefaultSaveOptions();
-
-		defaultSaveOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
-		defaultSaveOptions.put(XMLResource.OPTION_SAVE_TYPE_INFORMATION,
-			new XMLSave.XMLTypeInfo() {
-
-				public boolean shouldSaveType(EClass objectType,
-						EClassifier featureType, EStructuralFeature feature) {
-					return true;
-				}
-
-				public boolean shouldSaveType(EClass objectType,
-						EClass featureType, EStructuralFeature feature) {
-					return true;
-				}
-
-			});
+		resource.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
 
 		return resource;
 	}
