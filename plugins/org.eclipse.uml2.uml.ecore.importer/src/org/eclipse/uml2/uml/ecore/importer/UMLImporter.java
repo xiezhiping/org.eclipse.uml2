570c55b27f10ef0347f3c31f72d73c4634d963d1
khussey <khussey>
2007-01-04 18:55:29 +0000

    [169183] Adding missing cast and eliminating Java 5 warnings.

--- plugins/org.eclipse.uml2.uml.ecore.importer/src/org/eclipse/uml2/uml/ecore/importer/UMLImporter.java
+++ plugins/org.eclipse.uml2.uml.ecore.importer/src/org/eclipse/uml2/uml/ecore/importer/UMLImporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: UMLImporter.java,v 1.6 2007/01/04 18:05:36 khussey Exp $
+ * $Id: UMLImporter.java,v 1.7 2007/01/04 18:55:29 khussey Exp $
  */
 package org.eclipse.uml2.uml.ecore.importer;
 
