2bb038b0f6876841d9f24e5661e6ccf49863a96d
khussey <khussey>
2007-02-14 19:57:25 +0000

    [160679] Eliminating empty block warnings.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/CentralBufferNode.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/CentralBufferNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: CentralBufferNode.java,v 1.2 2006/05/12 20:38:04 khussey Exp $
+ * $Id: CentralBufferNode.java,v 1.3 2007/02/14 19:57:26 khussey Exp $
  */
 package org.eclipse.uml2.uml;
 
@@ -28,4 +28,7 @@ package org.eclipse.uml2.uml;
  */
 public interface CentralBufferNode
 		extends ObjectNode {
+
+	// no methods
+
 } // CentralBufferNode
--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/Realization.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/Realization.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: Realization.java,v 1.1 2005/11/14 22:26:00 khussey Exp $
+ * $Id: Realization.java,v 1.2 2007/02/14 19:57:26 khussey Exp $
  */
 package org.eclipse.uml2.uml;
 
@@ -28,4 +28,7 @@ package org.eclipse.uml2.uml;
  */
 public interface Realization
 		extends Abstraction {
+
+	// no methods
+
 } // Realization
