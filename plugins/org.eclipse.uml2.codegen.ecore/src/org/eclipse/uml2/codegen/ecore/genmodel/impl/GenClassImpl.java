133c557e07fa17348b55825ca6cdb7a3e3cf15c5
khussey <khussey>
2007-05-10 20:10:05 +0000

    [186264] Ensuring that operations that do not collide with implemented features are NOT considered implemented.

--- plugins/org.eclipse.uml2.codegen.ecore/src/org/eclipse/uml2/codegen/ecore/genmodel/impl/GenClassImpl.java
+++ plugins/org.eclipse.uml2.codegen.ecore/src/org/eclipse/uml2/codegen/ecore/genmodel/impl/GenClassImpl.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: GenClassImpl.java,v 1.39 2007/05/10 14:24:21 khussey Exp $
+ * $Id: GenClassImpl.java,v 1.40 2007/05/10 20:10:05 khussey Exp $
  */
 package org.eclipse.uml2.codegen.ecore.genmodel.impl;
 
@@ -347,8 +347,7 @@ public class GenClassImpl
 
 	@Override
 	public List<GenOperation> getImplementedGenOperations() {
-		return getImplementedGenOperations(new CollidingGenOperationFilter(
-			getImplementedGenFeatures()) {
+		return getImplementedGenOperations(new CollidingGenOperationFilter() {
 
 			@Override
 			public boolean accept(GenOperation genOperation) {
@@ -1271,8 +1270,7 @@ public class GenClassImpl
 	}
 
 	public List<GenOperation> getImplementedRedefinedGenOperations() {
-		return getImplementedGenOperations(new CollidingGenOperationFilter(
-			getImplementedGenFeatures()) {
+		return getImplementedGenOperations(new CollidingGenOperationFilter() {
 
 			@Override
 			public boolean accept(GenOperation genOperation) {
@@ -1350,13 +1348,9 @@ public class GenClassImpl
 			org.eclipse.emf.codegen.ecore.genmodel.impl.GenClassImpl.CollidingGenOperationFilter {
 
 		protected CollidingGenOperationFilter() {
-			this(getAllDuplicateGenFeatures());
-		}
-
-		protected CollidingGenOperationFilter(List<GenFeature> genFeatures) {
 			super();
 
-			allGenFeatures = genFeatures;
+			allGenFeatures = getAllDuplicateGenFeatures();
 		}
 
 	}
