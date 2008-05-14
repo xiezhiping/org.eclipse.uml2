e81e092c23879b5d2616304e511c812376df8540
jbruck <jbruck>
2008-05-14 15:18:40 +0000

    [214962] Port.getRequireds() should consider usages to other interfaces.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/internal/operations/PortOperations.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/internal/operations/PortOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *   IBM - initial API and implementation
  *
- * $Id: PortOperations.java,v 1.16 2008/01/15 22:11:59 jbruck Exp $
+ * $Id: PortOperations.java,v 1.17 2008/05/14 15:18:40 jbruck Exp $
  */
 package org.eclipse.uml2.uml.internal.operations;
 
@@ -239,7 +239,7 @@ public class PortOperations
 		Type type = (Type) port.eGet(UMLPackage.Literals.TYPED_ELEMENT__TYPE,
 			false);
 
-		if (type instanceof Classifier && !(type instanceof Interface)) {
+		if (type instanceof Classifier) {
 			Classifier classifier = (Classifier) port.getType();
 			ComponentOperations.usedInterfaces(null, classifier, false,
 				requireds);
