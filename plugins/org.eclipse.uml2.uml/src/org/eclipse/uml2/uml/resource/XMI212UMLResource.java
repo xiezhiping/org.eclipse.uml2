e5c4d26f11b7e43ed078c83c7be3d22af8a41afb
khussey <khussey>
2008-11-04 14:28:39 +0000

    [232332] Making XMI212UMLResource extend XMI2UMLResource.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/XMI212UMLResource.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/resource/XMI212UMLResource.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2008 IBM Corporation, Embarcadero Technologies, and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - initial API and implementation
+ *   Kenn Hussey (Embarcadero Technologies) - 232332
+ *
+ * $Id: XMI212UMLResource.java,v 1.2 2008/11/04 14:28:39 khussey Exp $
+ */
 package org.eclipse.uml2.uml.resource;
 
 import org.eclipse.emf.ecore.resource.Resource;
@@ -11,7 +24,7 @@ import org.eclipse.uml2.uml.internal.resource.XMI212UMLResourceFactoryImpl;
  * @since 3.0
  */
 public interface XMI212UMLResource
-		extends UMLResource {
+		extends XMI2UMLResource {
 
 	public interface Factory
 			extends Resource.Factory {
