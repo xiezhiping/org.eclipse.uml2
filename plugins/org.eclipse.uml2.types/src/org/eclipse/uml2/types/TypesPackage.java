1514d8a594457c65a92bd3950d379e3feaae0682
Kenn Hussey <Kenn.Hussey@gmail.com>
2011-08-21 22:17:32 -0400

    [327039] Adding implementation of UML 2.4 primitive types.

---
+++ plugins/org.eclipse.uml2.types/src/org/eclipse/uml2/types/TypesPackage.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (c) 2011 CEA and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   CEA - initial API and implementation
+ *
+ */
+package org.eclipse.uml2.types;
+
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Package</b> for the model.
+ * It contains accessors for the meta objects to represent
+ * <ul>
+ *   <li>each class,</li>
+ *   <li>each feature of each class,</li>
+ *   <li>each enum,</li>
+ *   <li>and each data type</li>
+ * </ul>
+ * <!-- end-user-doc -->
+ * @see org.eclipse.uml2.types.TypesFactory
+ * @model kind="package"
+ * @generated
+ */
+public interface TypesPackage
+		extends EPackage {
+
+	/**
+	 * The package name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNAME = "types"; //$NON-NLS-1$
+
+	/**
+	 * The package namespace URI.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNS_URI = "http://www.eclipse.org/uml2/4.0.0/Types"; //$NON-NLS-1$
+
+	/**
+	 * The package namespace name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eNS_PREFIX = "types"; //$NON-NLS-1$
+
+	/**
+	 * The singleton instance of the package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	TypesPackage eINSTANCE = org.eclipse.uml2.types.internal.impl.TypesPackageImpl
+		.init();
+
+	/**
+	 * The meta object id for the '<em>Boolean</em>' data type.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getBoolean()
+	 * @generated
+	 */
+	int BOOLEAN = 0;
+
+	/**
+	 * The meta object id for the '<em>Integer</em>' data type.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getInteger()
+	 * @generated
+	 */
+	int INTEGER = 1;
+
+	/**
+	 * The meta object id for the '<em>Real</em>' data type.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getReal()
+	 * @generated
+	 */
+	int REAL = 2;
+
+	/**
+	 * The meta object id for the '<em>String</em>' data type.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see java.lang.String
+	 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getString()
+	 * @generated
+	 */
+	int STRING = 3;
+
+	/**
+	 * The meta object id for the '<em>Unlimited Natural</em>' data type.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getUnlimitedNatural()
+	 * @generated
+	 */
+	int UNLIMITED_NATURAL = 4;
+
+	/**
+	 * Returns the meta object for data type '<em>Boolean</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for data type '<em>Boolean</em>'.
+	 * @model instanceClass="boolean"
+	 * @generated
+	 */
+	EDataType getBoolean();
+
+	/**
+	 * Returns the meta object for data type '<em>Integer</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for data type '<em>Integer</em>'.
+	 * @model instanceClass="int"
+	 * @generated
+	 */
+	EDataType getInteger();
+
+	/**
+	 * Returns the meta object for data type '<em>Real</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for data type '<em>Real</em>'.
+	 * @model instanceClass="double"
+	 * @generated
+	 */
+	EDataType getReal();
+
+	/**
+	 * Returns the meta object for data type '{@link java.lang.String <em>String</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for data type '<em>String</em>'.
+	 * @see java.lang.String
+	 * @model instanceClass="java.lang.String"
+	 * @generated
+	 */
+	EDataType getString();
+
+	/**
+	 * Returns the meta object for data type '<em>Unlimited Natural</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for data type '<em>Unlimited Natural</em>'.
+	 * @model instanceClass="int"
+	 * @generated
+	 */
+	EDataType getUnlimitedNatural();
+
+	/**
+	 * Returns the factory that creates the instances of the model.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the factory that creates the instances of the model.
+	 * @generated
+	 */
+	TypesFactory getTypesFactory();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * Defines literals for the meta objects that represent
+	 * <ul>
+	 *   <li>each class,</li>
+	 *   <li>each feature of each class,</li>
+	 *   <li>each enum,</li>
+	 *   <li>and each data type</li>
+	 * </ul>
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	interface Literals {
+
+		/**
+		 * The meta object literal for the '<em>Boolean</em>' data type.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getBoolean()
+		 * @generated
+		 */
+		EDataType BOOLEAN = eINSTANCE.getBoolean();
+
+		/**
+		 * The meta object literal for the '<em>Integer</em>' data type.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getInteger()
+		 * @generated
+		 */
+		EDataType INTEGER = eINSTANCE.getInteger();
+
+		/**
+		 * The meta object literal for the '<em>Real</em>' data type.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getReal()
+		 * @generated
+		 */
+		EDataType REAL = eINSTANCE.getReal();
+
+		/**
+		 * The meta object literal for the '<em>String</em>' data type.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see java.lang.String
+		 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getString()
+		 * @generated
+		 */
+		EDataType STRING = eINSTANCE.getString();
+
+		/**
+		 * The meta object literal for the '<em>Unlimited Natural</em>' data type.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see org.eclipse.uml2.types.internal.impl.TypesPackageImpl#getUnlimitedNatural()
+		 * @generated
+		 */
+		EDataType UNLIMITED_NATURAL = eINSTANCE.getUnlimitedNatural();
+
+	}
+
+} //TypesPackage
---
+++ plugins/org.eclipse.uml2.types/src/org/eclipse/uml2/types/internal/impl/TypesFactoryImpl.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright (c) 2011 CEA and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   CEA - initial API and implementation
+ *
+ */
+package org.eclipse.uml2.types.internal.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.uml2.types.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TypesFactoryImpl
+		extends EFactoryImpl
+		implements TypesFactory {
+
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static TypesFactory init() {
+		try {
+			TypesFactory theTypesFactory = (TypesFactory) EPackage.Registry.INSTANCE
+				.getEFactory("http://www.eclipse.org/uml2/4.0.0/Types"); //$NON-NLS-1$ 
+			if (theTypesFactory != null) {
+				return theTypesFactory;
+			}
+		} catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new TypesFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TypesFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			default :
+				throw new IllegalArgumentException(
+					"The class '" + eClass.getName() + "' is not a valid classifier"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object createFromString(EDataType eDataType, String initialValue) {
+		switch (eDataType.getClassifierID()) {
+			case TypesPackage.BOOLEAN :
+				return createBooleanFromString(eDataType, initialValue);
+			case TypesPackage.INTEGER :
+				return createIntegerFromString(eDataType, initialValue);
+			case TypesPackage.REAL :
+				return createRealFromString(eDataType, initialValue);
+			case TypesPackage.STRING :
+				return createStringFromString(eDataType, initialValue);
+			case TypesPackage.UNLIMITED_NATURAL :
+				return createUnlimitedNaturalFromString(eDataType, initialValue);
+			default :
+				throw new IllegalArgumentException(
+					"The datatype '" + eDataType.getName() + "' is not a valid classifier"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String convertToString(EDataType eDataType, Object instanceValue) {
+		switch (eDataType.getClassifierID()) {
+			case TypesPackage.BOOLEAN :
+				return convertBooleanToString(eDataType, instanceValue);
+			case TypesPackage.INTEGER :
+				return convertIntegerToString(eDataType, instanceValue);
+			case TypesPackage.REAL :
+				return convertRealToString(eDataType, instanceValue);
+			case TypesPackage.STRING :
+				return convertStringToString(eDataType, instanceValue);
+			case TypesPackage.UNLIMITED_NATURAL :
+				return convertUnlimitedNaturalToString(eDataType, instanceValue);
+			default :
+				throw new IllegalArgumentException(
+					"The datatype '" + eDataType.getName() + "' is not a valid classifier"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public Boolean createBooleanFromString(EDataType eDataType,
+			String initialValue) {
+		return initialValue == null
+			? null
+			: Boolean.valueOf(initialValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String convertBooleanToString(EDataType eDataType,
+			Object instanceValue) {
+		return instanceValue == null
+			? null
+			: instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public Integer createIntegerFromString(EDataType eDataType,
+			String initialValue) {
+		return initialValue == null
+			? null
+			: Integer.valueOf(initialValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String convertIntegerToString(EDataType eDataType,
+			Object instanceValue) {
+		return instanceValue == null
+			? null
+			: instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public Double createRealFromString(EDataType eDataType, String initialValue) {
+		return initialValue == null
+			? null
+			: Double.valueOf(initialValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String convertRealToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null
+			? null
+			: instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String createStringFromString(EDataType eDataType,
+			String initialValue) {
+		return initialValue;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String convertStringToString(EDataType eDataType,
+			Object instanceValue) {
+		return (String) instanceValue;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public Integer createUnlimitedNaturalFromString(EDataType eDataType,
+			String initialValue) {
+		return initialValue == null
+			? null
+			: ("*".equals(initialValue) //$NON-NLS-1$
+				? new Integer(-1)
+				: Integer.valueOf(initialValue));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	public String convertUnlimitedNaturalToString(EDataType eDataType,
+			Object instanceValue) {
+		return instanceValue == null
+			? null
+			: (((Integer) instanceValue).intValue() == -1
+				? "*" //$NON-NLS-1$
+				: instanceValue.toString());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TypesPackage getTypesPackage() {
+		return (TypesPackage) getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static TypesPackage getPackage() {
+		return TypesPackage.eINSTANCE;
+	}
+
+} //TypesFactoryImpl
