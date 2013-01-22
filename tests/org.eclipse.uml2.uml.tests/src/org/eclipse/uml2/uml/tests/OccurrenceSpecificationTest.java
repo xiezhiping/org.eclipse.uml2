7747211aa7b994021976ed6821180f85f148b4a1
Kenn Hussey <Kenn.Hussey@gmail.com>
2013-01-22 15:39:24 -0500

    [297216] Adding tests for OccurrenceSpecification::covered.

--- tests/org.eclipse.uml2.uml.tests/src/org/eclipse/uml2/uml/tests/OccurrenceSpecificationTest.java
+++ tests/org.eclipse.uml2.uml.tests/src/org/eclipse/uml2/uml/tests/OccurrenceSpecificationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation, CEA, and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,13 +7,14 @@
  *
  * Contributors:
  *   IBM - initial API and implementation
+ *   Kenn Hussey (CEA) - 297216
  *
- * $Id: OccurrenceSpecificationTest.java,v 1.3 2007/02/14 20:06:13 khussey Exp $
  */
 package org.eclipse.uml2.uml.tests;
 
 import junit.textui.TestRunner;
 
+import org.eclipse.uml2.uml.Lifeline;
 import org.eclipse.uml2.uml.OccurrenceSpecification;
 
 import org.eclipse.uml2.uml.UMLFactory;
@@ -22,6 +23,13 @@ import org.eclipse.uml2.uml.UMLFactory;
  * <!-- begin-user-doc -->
  * A test case for the model object '<em><b>Occurrence Specification</b></em>'.
  * <!-- end-user-doc -->
+ * <p>
+ * The following operations are tested:
+ * <ul>
+ *   <li>{@link org.eclipse.uml2.uml.OccurrenceSpecification#getCovered() <em>Get Covered</em>}</li>
+ *   <li>{@link org.eclipse.uml2.uml.OccurrenceSpecification#setCovered(org.eclipse.uml2.uml.Lifeline) <em>Set Covered</em>}</li>
+ * </ul>
+ * </p>
  * @generated
  */
 public class OccurrenceSpecificationTest
@@ -79,6 +87,62 @@ public class OccurrenceSpecificationTest
 	protected void tearDown()
 			throws Exception {
 		setFixture(null);
+	}
+
+	/**
+	 * Tests the '{@link org.eclipse.uml2.uml.OccurrenceSpecification#getCovered() <em>Get Covered</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.uml.OccurrenceSpecification#getCovered()
+	 * @generated NOT
+	 */
+	public void testGetCovered() {
+		assertNull(getFixture().getCovered());
+
+		Lifeline covered = UMLFactory.eINSTANCE.createLifeline();
+		getFixture().getCovereds().add(0, covered);
+
+		assertEquals(covered, getFixture().getCovered());
+
+		getFixture().getCovereds().add(0,
+			covered = UMLFactory.eINSTANCE.createLifeline());
+
+		assertEquals(covered, getFixture().getCovered());
+
+		getFixture().getCovereds().clear();
+
+		assertNull(getFixture().getCovered());
+	}
+
+	/**
+	 * Tests the '{@link org.eclipse.uml2.uml.OccurrenceSpecification#setCovered(org.eclipse.uml2.uml.Lifeline) <em>Set Covered</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.uml2.uml.OccurrenceSpecification#setCovered(org.eclipse.uml2.uml.Lifeline)
+	 * @generated NOT
+	 */
+	public void testSetCovered__Lifeline() {
+		assertNull(getFixture().getCovered());
+		assertEquals(0, getFixture().getCovereds().size());
+
+		Lifeline covered = UMLFactory.eINSTANCE.createLifeline();
+		getFixture().setCovered(covered);
+
+		assertEquals(covered, getFixture().getCovered());
+		assertEquals(1, getFixture().getCovereds().size());
+		assertEquals(covered, getFixture().getCovereds().get(0));
+
+		getFixture()
+			.setCovered(covered = UMLFactory.eINSTANCE.createLifeline());
+
+		assertEquals(covered, getFixture().getCovered());
+		assertEquals(1, getFixture().getCovereds().size());
+		assertEquals(covered, getFixture().getCovereds().get(0));
+
+		getFixture().setCovered(null);
+
+		assertNull(getFixture().getCovered());
+		assertEquals(0, getFixture().getCovereds().size());
 	}
 
 } //OccurrenceSpecificationTest
