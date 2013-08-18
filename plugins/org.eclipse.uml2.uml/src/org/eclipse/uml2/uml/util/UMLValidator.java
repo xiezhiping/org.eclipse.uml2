3cbafe7b5750ad0ed4b92edafeb06842dfa83765
Kenn Hussey <Kenn.Hussey@gmail.com>
2013-08-18 13:14:40 -0400

    [414970] Updating based on changes in EMF for bug 408609.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLValidator.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLValidator.java
@@ -9,7 +9,7 @@
  *   IBM - initial API and implementation
  *   Kenn Hussey (Embarcadero Technologies) - 205188, 204200
  *   Kenn Hussey - 286329, 320318, 323000, 323181, 354453
- *   Kenn Hussey (CEA) - 327039, 351774, 297216, 212765, 408610
+ *   Kenn Hussey (CEA) - 327039, 351774, 297216, 212765, 408610, 414970
  *
  */
 package org.eclipse.uml2.uml.util;
@@ -26224,15 +26224,11 @@ public class UMLValidator
 			EAnnotation eAnnotation = eClassesIterator.next().getEAnnotation(
 				"duplicates"); //$NON-NLS-1$
 			if (eAnnotation != null) {
-				for (EObject eContents : eAnnotation.eContents()) {
-					if (eContents instanceof EAnnotation) {
-						EAnnotation nestedEAnnotation = (EAnnotation) eContents;
-						if (featureName.equals(nestedEAnnotation.getSource())) {
-							redefinitionDetail = nestedEAnnotation.getDetails()
-								.get(key);
-							break;
-						}
-					}
+				EAnnotation nestedEAnnotation = eAnnotation
+					.getEAnnotation(featureName);
+				if (nestedEAnnotation != null) {
+					redefinitionDetail = nestedEAnnotation.getDetails()
+						.get(key);
 				}
 			}
 		}
