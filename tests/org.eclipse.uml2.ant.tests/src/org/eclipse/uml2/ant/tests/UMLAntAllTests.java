b59bb0c7175a58e4d70c4df99534d3b7b257fb55
Christian W. Damus <give.a.damus@gmail.com>
2014-01-08 09:33:17 -0500

    [286404] Ant Task for Profile Definition https://bugs.eclipse.org/bugs/show_bug.cgi?id=286404
    Address code review comments:  remove the 'uml' segment from bundle symbolic name and package namespaces in the new Ant plug-ins.

---
+++ tests/org.eclipse.uml2.ant.tests/src/org/eclipse/uml2/ant/tests/UMLAntAllTests.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013, 2014 CEA and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Christian W. Damus (CEA) - initial API and implementation
+ *   Christian W. Damus (CEA) - 286404
+ *
+ */
+package org.eclipse.uml2.ant.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * The master test suite for the UML Ant Tasks tests.
+ */
+public class UMLAntAllTests
+		extends TestSuite {
+
+	/**
+	 * Initializes me with my name.
+	 */
+	public UMLAntAllTests(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		TestSuite suite = new UMLAntAllTests("UML Ant Tasks Tests"); //$NON-NLS-1$
+		suite.addTestSuite(UMLDefineProfileTaskTest.class);
+		return suite;
+	}
+
+}
