0b6660d2371db84b100d1ffb4dc9bd6aa9948461
Kenn Hussey <Kenn.Hussey@gmail.com>
2014-11-05 15:20:38 -0500

    [446388] Optimizing lookup of named elements.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
@@ -10,7 +10,7 @@
  *   Kenn Hussey (Embarcadero Technologies) - 199624, 184249, 204406, 208125, 204200, 213218, 213903, 220669, 208016, 226396, 271470
  *   Nicolas Rouquette (JPL) - 260120, 313837
  *   Kenn Hussey - 286329, 313601, 314971, 344907, 236184, 335125
- *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915
+ *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915, 446388
  *   Yann Tanguy (CEA) - 350402
  *   Christian W. Damus (CEA) - 392833, 251963, 405061, 409396, 176998, 180744, 403374, 416833, 420338, 405065, 431342
  *   E.D.Willink - 420338
@@ -11393,13 +11393,14 @@ public class UMLUtil
 
 		if (index == -1) {
 
-			for (NE member : EcoreUtil.<NE> getObjectsByType(eObjects, eClass)) {
+			for (NE namedElement : EcoreUtil.<NE> getObjectsByType(eObjects,
+				eClass)) {
 
 				if (ignoreCase
-					? qualifiedName.equalsIgnoreCase(member.getName())
-					: qualifiedName.equals(member.getName())) {
+					? qualifiedName.equalsIgnoreCase(namedElement.getName())
+					: qualifiedName.equals(namedElement.getName())) {
 
-					namedElements.add(member);
+					namedElements.add(namedElement);
 				}
 			}
 		} else {
@@ -11407,14 +11408,15 @@ public class UMLUtil
 			qualifiedName = qualifiedName.substring(index
 				+ NamedElement.SEPARATOR.length());
 
-			for (Namespace namespace : EcoreUtil.<Namespace> getObjectsByType(
-				eObjects, UMLPackage.Literals.NAMESPACE)) {
+			for (NamedElement namedElement : EcoreUtil
+				.<NamedElement> getObjectsByType(eObjects,
+					UMLPackage.Literals.NAMED_ELEMENT)) {
 
 				if (ignoreCase
-					? name.equalsIgnoreCase(namespace.getName())
-					: name.equals(namespace.getName())) {
+					? name.equalsIgnoreCase(namedElement.getName())
+					: name.equals(namedElement.getName())) {
 
-					findNamedElements(namespace.getMembers(), qualifiedName,
+					findNamedElements(namedElement.eContents(), qualifiedName,
 						ignoreCase, eClass, namedElements);
 				}
 			}
