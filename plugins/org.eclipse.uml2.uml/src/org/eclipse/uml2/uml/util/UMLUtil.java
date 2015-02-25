02df3d8a0adae39fab85ea9b3bec96507a4757d9
Kenn Hussey <Kenn.Hussey@gmail.com>
2015-02-24 20:02:27 -0500

    [458906] Ensuring capability annotation sources are valid URIs.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
@@ -10,7 +10,7 @@
  *   Kenn Hussey (Embarcadero Technologies) - 199624, 184249, 204406, 208125, 204200, 213218, 213903, 220669, 208016, 226396, 271470
  *   Nicolas Rouquette (JPL) - 260120, 313837
  *   Kenn Hussey - 286329, 313601, 314971, 344907, 236184, 335125
- *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915, 446388, 454864
+ *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915, 446388, 454864, 458906
  *   Yann Tanguy (CEA) - 350402
  *   Christian W. Damus (CEA) - 392833, 251963, 405061, 409396, 176998, 180744, 403374, 416833, 420338, 405065, 431342
  *   E.D.Willink - 420338
@@ -2928,10 +2928,18 @@ public class UMLUtil
 											capabilityElement}));
 							}
 
+							String qualifiedName = capabilityElement
+								.getQualifiedName();
+
+							if (!UML2Util.isEmpty(qualifiedName)) {
+								qualifiedName = qualifiedName.replace(
+									NamedElement.SEPARATOR, "/"); //$NON-NLS-1$
+							}
+
 							EAnnotation eAnnotation = getEAnnotation(
 								getEAnnotation(receivingElement,
 									UML2_UML_PACKAGE_2_0_NS_URI, true),
-								capabilityElement.getQualifiedName(), true);
+								qualifiedName, true);
 
 							eAnnotation.getReferences().add(resultingEObject);
 
@@ -8013,7 +8021,8 @@ public class UMLUtil
 						.getEAnnotations()) {
 
 						if (isCapabilityAnnotation(subAnnotation)) {
-							String capName = subAnnotation.getSource();
+							String capName = subAnnotation.getSource().replace(
+								"/", NamedElement.SEPARATOR); //$NON-NLS-1$
 							String capURI = subAnnotation.getDetails().get(
 								ANNOTATION_DETAIL__URI);
 
