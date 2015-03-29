0d202ff4ded2bec3cab5adfb4bccab92aa8f5676
Kenn Hussey <Kenn.Hussey@gmail.com>
2015-03-29 19:54:04 -0400

    [463066] Ensuring generated profile package definitions are found.

--- plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
+++ plugins/org.eclipse.uml2.uml/src/org/eclipse/uml2/uml/util/UMLUtil.java
@@ -10,7 +10,7 @@
  *   Kenn Hussey (Embarcadero Technologies) - 199624, 184249, 204406, 208125, 204200, 213218, 213903, 220669, 208016, 226396, 271470
  *   Nicolas Rouquette (JPL) - 260120, 313837
  *   Kenn Hussey - 286329, 313601, 314971, 344907, 236184, 335125
- *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915, 446388, 454864, 458906, 461374
+ *   Kenn Hussey (CEA) - 327039, 358792, 364419, 366350, 307343, 382637, 273949, 389542, 389495, 316165, 392833, 399544, 322715, 163556, 212765, 397324, 204658, 408612, 411731, 269598, 422000, 416833, 424568, 427167, 418466, 419324, 429994, 433157, 439915, 446388, 454864, 458906, 461374, 463066
  *   Yann Tanguy (CEA) - 350402
  *   Christian W. Damus (CEA) - 392833, 251963, 405061, 409396, 176998, 180744, 403374, 416833, 420338, 405065, 431342
  *   E.D.Willink - 420338
@@ -11715,7 +11715,31 @@ public class UMLUtil
 
 			return null;
 		} else if (definition instanceof EPackage) {
-			return getProfile((EPackage) definition, context);
+			EPackage ePackage = (EPackage) definition;
+			Profile profile = getProfile(ePackage, context);
+
+			if (profile == null) {
+				EPackage eSuperPackage = ePackage.getESuperPackage();
+
+				if (eSuperPackage != null) {
+					org.eclipse.uml2.uml.Package nestingPackage = (org.eclipse.uml2.uml.Package) getNamedElement(
+						eSuperPackage, context);
+
+					if (nestingPackage != null) {
+						String name = ePackage.getName();
+
+						for (org.eclipse.uml2.uml.Package nestedPackage : nestingPackage
+							.getNestedPackages()) {
+
+							if (safeEquals(getEcoreName(nestedPackage), name)) {
+								return nestedPackage;
+							}
+						}
+					}
+				}
+			}
+
+			return profile;
 		} else {
 			return null;
 		}
